2a840590dedb050c55ff46dc008e1a359547a1e8
Melwyn <melwynt@gmail.com>
2022-03-05 18:57:22 +0100

    :rocket: Fetch advice from Advice Slip API (#2)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+  const [advice, setAdvice] = useState(null);
+
+  const getData = async () => {
+    fetch('https://api.adviceslip.com/advice/117')
+      .then((res) => {
+        if (res.ok) {
+          console.log(res);
+          return res.json();
+        } else {
+          console.log('Not successful');
+        }
+      })
+      .then((res) => {
+        console.log(res);
+        setAdvice(res.slip);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <main>
       <div className="card">
-        <h1 className="card__title">ADVICE #117</h1>
+        <h1 className="card__title">ADVICE #{advice ? advice.id : ''}</h1>
         <div className="card__advice">
           “It is easy to sit up and take notice, what's difficult is getting up
           and taking action.”
+          {/* {advice ? advice.advice : ''} */}
         </div>
         <img
           src={require('../images/pattern-divider-desktop.svg')}
